1177f2555db40c67f152dc40ed734dac2ba0ad0d
hossam <hossamamer910@gmail.com>
2023-09-20 18:54:33 +0300

    update enviroment

--- src/app/MovieModule/Service/movie.service.ts
+++ src/app/MovieModule/Service/movie.service.ts
@@ -1,9 +1,62 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MasterService } from 'src/app/Service/master.service';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  base: string = 'Movies';
+
+  type: string = 'Types';
+
+  director: string = 'Directors';
+
+  movieType: string = 'MovieTypes';
+
+  url: string = '';
+
+  movieDetails: string = 'Details';
+
+  movieUpdate: string = 'Update';
+
+  constructor(
+    private master: MasterService,
+    private http: HttpClient
+  ) { this.url = environment.UrlBase; }
+
+  getAllTypes() {
+    return this.master.getAll(this.type);
+  }
+
+  getAllDirectors() {
+    return this.master.getAll(this.director);
+  }
+
+  getAllMovieTypes() {
+    return this.master.getAll(this.movieType);
+  }
+
+  getAllMovies() {
+    return this.master.getAll(this.base);
+  }
+
+  getMovieDetails(id: any) {
+    return this.master.getById(this.base, this.movieDetails, id);
+  }
+
+  addMovie(data: any) {
+    return this.master.Add(this.base, data);
+  }
+
+  updateMovie(id: any, data: any) {
+    return this.master.Update(this.base, this.movieUpdate, id, data);
+  }
+
+  deleteMovie(id: any) {
+    return this.master.Delete(this.base, id);
+  }
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    UrlBase:'https://localhost:7292/api/'
+    UrlBase:'https://localhost:7292/api/',
+    ImageBaseUrl:'https://localhost:7292'
 };
